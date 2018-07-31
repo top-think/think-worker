8bbb72a5f72d2913753dad932b6b5cd2e6bec2cb
thinkphp <thinkphp@qq.com>
2018-07-31 16:06:47 +0800

    注释调整

--- src/config/server.php
+++ src/config/server.php
@@ -20,7 +20,7 @@ return [
     'port'           => 2345, // 监听端口
     'socket'         => '', // 完整监听地址
     'context'        => [], // socket 上下文选项
-    'worker_class'   => '', // 自定义Workerman服务类名
+    'worker_class'   => '', // 自定义Workerman服务类名 支持数组定义多个服务
 
     // 支持workerman的所有配置参数
     'name'           => 'thinkphp',
