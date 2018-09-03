caf8b97b47a3c22a31cbbbea854f45e690dbd37b
thinkphp <thinkphp@qq.com>
2018-09-03 17:02:40 +0800

    改进

--- src/command.php
+++ src/command.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 
 \think\Console::addDefaultCommands([
-    'worker:gateway' => '\\think\\worker\\command\\GatewayWorker',
-    'worker:server'  => '\\think\\worker\\command\\Server',
     'worker'         => '\\think\\worker\\command\\Worker',
+    'worker:server'  => '\\think\\worker\\command\\Server',
+    'worker:gateway' => '\\think\\worker\\command\\GatewayWorker',
 ]);
